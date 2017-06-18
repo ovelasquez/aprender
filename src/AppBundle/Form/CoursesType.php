d3ae41b95d8a6a86a5af24a3c55312ea4438827a
Virgilio Centeno <centeno365.vc@gmail.com>
2017-06-17 22:22:09 -0400

    Correccion vista Curso

--- src/AppBundle/Form/CoursesType.php
+++ src/AppBundle/Form/CoursesType.php
@@ -12,7 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class CoursesType extends AbstractType {
@@ -23,22 +22,30 @@ class CoursesType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
 
         $builder
-                ->add('category', null, array('required' => true, 'attr' => array('class' => 'form-control', 'placeholder' => 'Categorías',)))
+                ->add('category', EntityType::class, array(
+                    'class' => 'AppBundle:Coursecategories',
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('u')
+                                ->orderBy('u.name', 'ASC');
+                    },
+                    'placeholder' => 'Seleccione',
+                    'required' => true,
+                ))
                 ->add('title', TextType::class, array('required' => true, 'attr' => array('class' => 'form-control', 'placeholder' => 'Escribe algo distinto, que llame la atención')))
                 ->add('description', TextareaType::class, array('required' => true, 'attr' => array('class' => 'form-control', 'placeholder' => 'En 140 caracteres escribe sobre tu curso. Esta descripción estará en la página principal.')))
                 ->add('resume', TextareaType::class, array('required' => true, 'attr' => array('class' => 'form-control', 'placeholder' => 'Este es el momento que todos esperan, en 500 caracteres dino más sobre todo lo que quieras dar.')))
                 ->add('photo', FileType::class, array('data_class' => null, 'required' => true, 'attr' => array('class' => 'form-control-file', 'placeholder' => 'File')))
                 ->add('video', TextType::class, array('required' => false, 'attr' => array('class' => 'form-control', 'placeholder' => '')))
-                ->add('type', null, array('required' => true,'placeholder' => 'Seleccione', 'attr' => array('class' => 'form-control')))
-                ->add('place', ChoiceType::class, array('choices' => array($options['locations'][0] => '1', $options['locations'][1] => '2'), 'required' => true, 'attr' => array('class' => 'form-control')))
+                ->add('type', null, array('required' => true, 'placeholder' => 'Seleccione', 'attr' => array('class' => 'form-control')))
+                ->add('place', ChoiceType::class, array('choices' => array($options['locations'][0] => '1', $options['locations'][1] => '2'), 'required' => true, 'placeholder' => 'Seleccione', 'attr' => array('class' => 'form-control')))
                 ->add('country', CountryType::class, array('required' => true, 'attr' => array('class' => 'form-control')))
                 ->add('province', TextType::class)
                 ->add('avenue', TextType::class, array('required' => true, 'attr' => array('class' => 'form-control')))
                 ->add('address', TextareaType::class, array('required' => true, 'attr' => array('class' => 'form-control')))
                 ->add('knowledge', TextareaType::class, array('required' => true, 'attr' => array('class' => 'form-control',)))
                 ->add('tool', TextareaType::class, array('required' => true, 'attr' => array('class' => 'form-control',)))
-                ->add('minstudent', IntegerType::class, array('required' => true, 'attr' => array('class' => 'form-control',)))
-                ->add('maxstudent', IntegerType::class, array('required' => true, 'attr' => array('class' => 'form-control',)))
+                ->add('minstudent', TextType::class, array('required' => true, 'attr' => array('class' => 'form-control',)))
+                ->add('maxstudent', TextType::class, array('required' => true, 'attr' => array('class' => 'form-control',)))
                 ->add('startdate', TextType::class)
                 ->add('enddate', TextType::class)
                 ->add('hour', TextType::class)
@@ -50,7 +57,7 @@ class CoursesType extends AbstractType {
                         return $er->createQueryBuilder('u')
                                 ->orderBy('u.name', 'ASC');
                     },
-                    'placeholder' => 'Seleccione',                    
+                    'placeholder' => 'Seleccione',
                     'required' => true,
                 ))
         ;
